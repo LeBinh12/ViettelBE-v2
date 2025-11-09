5ba5d30514fc8712c935276ff353395a1fb6e3b0
phammytien <phmytien19022004@gmail.com>
2025-11-09 21:08:57 +0700

    Add files via upload

---
+++ Application/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<List<Customer>> GetAllAsync();
+        Task<Customer?> GetByEmailAsync(string email);
+        Task<Customer?> GetByIdAsync(Guid id);
+        Task AddAsync(Customer customer);
+        Task UpdateAsync(Customer customer);
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ Application/Interfaces/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<List<Customer>> GetAllAsync();
+        Task AddAsync(Customer customer);
+        Task UpdateAsync(Customer customer);
+        Task DeleteAsync(Guid id);
+        Task<bool> IsEmailValidAsync(string email);
+    }
+}

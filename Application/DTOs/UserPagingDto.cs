fde28fd413b3756a42a97584539a36530d2219b0
phammytien <phmytien19022004@gmail.com>
2025-11-11 22:13:54 +0700

    Add files via upload

---
+++ Application/DTOs/UserPagingDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.DTOs
+{
+    public class UserPagingRequest
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        //public string? Keyword { get; set; } // tìm kiếm theo username hoặc email
+    }
+
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Items { get; set; } = new List<T>();
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+    }
+}
